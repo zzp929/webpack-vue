b215dd40448dc78b186d8c820b4f8798d7bd2cf4
z_zp929 <853666954@qq.com>
2019-04-11 15:14:32 +0800

    'first'

---
+++ src/app.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="text">
+        {{text}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                text: 'hello'
+            }
+        }
+    }
+</script>
+
+<style>
+    #text {
+        color: red;
+    }
+</style>
