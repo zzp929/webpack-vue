d6529c6bd2574ae26e4268ce573ee4d81628e1b3
z_zp929 <853666954@qq.com>
2019-04-12 15:29:54 +0800

    111

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,20 @@ const path = require('path')
 const {VueLoaderPlugin} = require('vue-loader')
 
 module.exports = {
-    entry: path.join(__dirname, 'src/index.js'),
+    entry: path.join(__dirname, 'src/index.js'),   //入口文件   定义__dirname路径，保证绝对路径
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.js',//出口文件
         path: path.join(__dirname, 'dist')
     },
+    // resolve: {
+    //     extensions: ['.js', '.vue', '.json'],
+    //     alias: {
+    //         '@': resolve('src'),
+    //         'common': resolve('src/common'),
+    //         'components': resolve('src/components'),
+    //         'store': resolve('src/store'),
+    //     }
+    // },
     module: {
         rules: [
             {
@@ -15,11 +24,27 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                loader: 'style-loader!css-loader'
+                // loader: 'style-loader!css-loader'  //同下
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
             },
+            {
+                test: /\.(gif|jpg|jpeg|png|svg)$/,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options:{
+                            limit:1024,
+                            name:'[name]-aaa.[ext]'
+                        }
+                    }
+                ]
+            }
         ]
     },
     plugins: [
-        new VueLoaderPlugin()
+        new VueLoaderPlugin()   //vue-loaderzai 15.*之后的版本使用都是需要伴生vueLoaderPlugin
     ],
 }
